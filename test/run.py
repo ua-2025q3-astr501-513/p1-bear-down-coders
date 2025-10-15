28ed1c605659d5b1d2e52e49dceb49593e00bb19
Tyler Reese <tylerbreese@dhcp-10-134-155-204.uawifi.arizona.edu>
2025-10-15 11:53:07 -0700

    change to presentation, fixed test import

--- test/run.py
+++ test/run.py
@@ -1,4 +1,10 @@
+import sys
+import os
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','src','project'))
+sys.path.append(module_path)
+
 from orbit_integrator import orbit_integrator
+import numpy as np
 
 mu = 1.0
 h = 0.01
@@ -10,4 +16,6 @@ methods = ["leapfrog", "rk4", "yoshida4"]
 for m in methods:
     sim = orbit_integrator(mu=mu, h=h, method=m)
     sim.integrate(x0, v0, N)
-    sim.plot_orbit(show_energy=True)
+    sim.plot_orbit(show_energy=False)
+
+
